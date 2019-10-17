3b03a3bd602261e4bef89704d66788faefc59fc3
dpe <woltesn@gmail.com>
2019-10-17 17:18:25 +0300

    Selenium WEB driver+pytest+allure

---
+++ test_google_search.py
@@ -0,0 +1,23 @@
+from allure_commons.types import Severity
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.keys import Keys
+import time
+import allure
+
+@allure.title('Результат поиска')
+@allure.severity(Severity.BLOCKER)
+def test_google_search():
+
+    driver = WebDriver(executable_path='C:\dpe\Chromdriver\chromedriver.exe')
+    with allure.step('Открывем страницу поиска'):
+        driver.get('https://www.google.com/')  #Open Google.com in Google Chrome browser
+    find_serch = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input') #Search for input on a web page
+    with allure.step('Вводим название сайта'):
+        find_serch.send_keys('renarosh')
+        time.sleep(2)
+    with allure.step('Поиск кнопки Ввод'):
+        find_name = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[2]/div[2]/div[2]/center/input[1]')
+
+    find_name.click()
+
+
