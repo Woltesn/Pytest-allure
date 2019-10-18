8089cfe3494c12e0e26d6a7c268107c5743cdbb6
dpe <woltesn@gmail.com>
2019-10-18 10:18:36 +0300

    Selenium WEB driver+pytest+allure

--- test_google_search.py
+++ test_google_search.py
@@ -1,6 +1,7 @@
 from allure_commons.types import Severity
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.keys import Keys
+import nidaqmx
 import time
 import allure
 
@@ -17,7 +18,38 @@ def test_google_search():
         time.sleep(2)
     with allure.step('Поиск кнопки Ввод'):
         find_name = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[2]/div[2]/div[2]/center/input[1]')
-        print("Chec")
     find_name.click()
+    driver.close()
+
+
+def test_second():
+    with allure.step('Проверка аналогового входа'):
+        a=0
+        while a < 10:
+            with nidaqmx.Task() as task:
+                task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
+                print(task.read())
+                time.sleep(0.5)
+                assert task.read() > 1.5
+                a += 1
+                time.sleep(1)
+
+
+    # Command for generate Allure report file:
+    # "pytest test_google_search.py --alluredir=allure_results"
+    #
+    # Command for visualization Allure report in WEB page:
+    # "allure serve allure_results"
+
+def test_math_operation():
+    a = 3
+    b = 4
+    x = a+b
+    with allure.step('Проверка правильности сложения (2)'):
+        assert x == 7
+    with allure.step('Проверка правильности сложения'):
+        assert x == 6
+
+
 
 
